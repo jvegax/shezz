44a19fdafa212398dce7921ccf59e9a6567329ce
jvegax <toledo.utrera@hotmail.com>
2023-01-02 16:52:29 +0100

    index fixed

--- whoosh/init.py
+++ whoosh/init.py
@@ -1,12 +1,12 @@
 # Descripcion: este script genera el indice de whoosh
 
 from whoosh.index import create_in
-from whoosh.fields import Schema, TEXT, ID
+from whoosh.fields import Schema, TEXT, ID, NUMERIC
 import json, shutil, os, sys
 
 arguments = sys.argv
-SHEIN_INDEX_PATH = '/Users/jvegax/projects/python/shezz-env/shezz-repo/whoosh/Index'
-SHEIN_TOPS_DATA_PATH = '/Users/jvegax/projects/python/shezz-env/shezz-repo/data/shein/tops-shein.json'
+SHEZZ_INDEX_PATH = '/Users/jvegax/projects/python/shezz-env/shezz-repo/whoosh/Index'
+SHEZZ_TOPS_DATA_PATH = '/Users/jvegax/projects/python/shezz-env/shezz-repo/data/shezz/tops-shezz.json'
 ALL_PRODUCTS_DATA_PATH = '/Users/jvegax/projects/python/shezz-env/shezz-repo/data/all-products.json'
 
 # Creamos un esquema para nuestros documentos
@@ -14,8 +14,8 @@ def create_index():
     schema = Schema(
         name=TEXT(stored=True, phrase=False),
         sku = ID(stored=True),
-        price_discount=TEXT(stored=True),
-        price_original=TEXT(stored=True),
+        price_discount=NUMERIC(stored=True),
+        price_original=NUMERIC(stored=True),
         category=TEXT(stored=True),
         rating=TEXT(stored=True),
         product_link=TEXT(stored=True),
@@ -23,15 +23,15 @@ def create_index():
         images=TEXT(stored=True)
     )
     
-    if os.path.exists(SHEIN_INDEX_PATH):
-        shutil.rmtree(SHEIN_INDEX_PATH)
-    os.mkdir(SHEIN_INDEX_PATH)
+    if os.path.exists(SHEZZ_INDEX_PATH):
+        shutil.rmtree(SHEZZ_INDEX_PATH)
+    os.mkdir(SHEZZ_INDEX_PATH)
 
-    # Creamos un índice en un directorio llamado "shein_index"
-    shein_ix = create_in(SHEIN_INDEX_PATH, schema)
-    writer = shein_ix.writer()
+    # Creamos un índice en un directorio llamado "shezz_index"
+    shezz_ix = create_in(SHEZZ_INDEX_PATH, schema)
+    writer = shezz_ix.writer()
 
-    with open(SHEIN_TOPS_DATA_PATH, "r") as tops_file:
+    with open(ALL_PRODUCTS_DATA_PATH, "r") as tops_file:
         obj_list = json.load(tops_file)
         for obj in obj_list:
             writer.add_document(
--- whoosh/search.py
+++ whoosh/search.py
@@ -1,7 +1,6 @@
 # Descripcion: Este scritp contiene las funciones para busqueda sobre el indice de whoosh
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
-from whoosh.query import NumericRange
 import sys
 
 ARGS = sys.argv
@@ -18,20 +17,14 @@ def search_by_name(query):
         print("✅ Found {} results.".format(len(results)))
 
 
-def search_by_price(range_query):
+def search_by_price():
     # Abrimos el índice que acabamos de crear
     shezz_index = open_dir(SHEZZ_INDEX_PATH)
-
-    # Creamos una consulta de rango utilizando NumericRange
-    lower_bound, upper_bound = range_query.split("-")
-    query_res = NumericRange(
-        'price_original', lower_bound, upper_bound, include_lower=True, include_upper=True)
-
+    # Crea una consulta de rango para buscar productos con precio entre 10 y 20 con whoosh version 2.7.4
+    query = shezz_index.schema["price_discount"].range(10, 20)
     with shezz_index.searcher() as searcher:
-        # Realizamos la búsqueda utilizando la consulta de rango
-        results = searcher.search(query_res, limit=None)
-        for result in results:
-            print(result)
+        results = searcher.search(query)
         print("✅ Found {} results.".format(len(results)))
+         
         
-search_by_price(ARGS[1])
+search_by_price()
